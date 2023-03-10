ae94edfdee4ebf7ba5123e2a38ab8ea8591a181d
nulla2011 <nulla@outlook.com>
2023-03-10 23:35:18 +0800

    init

---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper container mx-auto absolute">
+    <div class="inline-block tab" id="idol">IDOL</div>
+    <div class="inline-block tab" id="comic">COMIC</div>
+  </div>
+</template>
+<script setup lang="ts">
+// import { TabsPaneContext } from "element-plus";
+// import { ref } from "vue";
+// const activeName = ref('first')
+
+</script>
+<style lang="scss"></style>
